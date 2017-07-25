7112a51bd2e5e05041527f8fcc43b56a7b2cfb5d
Muyuan <muyuanlin@outlook.com>
2017-07-24 23:28:14 -0400

    Remove build directory

--- build/lidar_camera_calib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/audren/lidar_camera_calib/calib_ws/src/lidar_camera_calib
-# Build directory: /home/audren/lidar_camera_calib/calib_ws/build/lidar_camera_calib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
